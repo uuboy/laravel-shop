b5200b8a06b0fe3e54dceddf8c0077f32b376cb6
uuboy <uu_boy@foxmail.com>
2018-08-14 12:32:21 +0000

    add_user_address

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,7 +6,6 @@
  */
 
 require('./bootstrap');
-
 window.Vue = require('vue');
 
 /**
@@ -16,7 +15,8 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 const app = new Vue({
     el: '#app'
 });
