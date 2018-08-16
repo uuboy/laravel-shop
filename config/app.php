8435f3c5a40dfc06782e157341e5d81342f04666
uuboy <uu_boy@foxmail.com>
2018-08-16 08:51:28 +0000

    close order

--- config/app.php
+++ config/app.php
@@ -126,6 +126,8 @@ return [
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
+    'order_ttl' => 1800,
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
