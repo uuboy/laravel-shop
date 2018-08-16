4ced07c9eb518b590d812fade767c3b507bac452
uuboy <uu_boy@foxmail.com>
2018-08-16 07:47:45 +0000

    favor list

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,12 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Auth::routes();
 
@@ -31,8 +30,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 
 });
-
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
