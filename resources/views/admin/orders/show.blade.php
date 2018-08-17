85ab9ec152b4597e5ad38398aa3d020724e86817
uuboy <uu_boy@foxmail.com>
2018-08-17 04:46:17 +0000

    refund agree

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -49,6 +49,7 @@
       <!-- 订单发货开始 -->
       <!-- 如果订单未发货，展示发货表单 -->
       @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+        @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS)
       <tr>
         <td colspan="4">
           <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
@@ -84,6 +85,7 @@
         <td>物流单号：</td>
         <td>{{ $order->ship_data['express_no'] }}</td>
       </tr>
+        @endif
       @endif
       <!-- 订单发货结束 -->
       @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
@@ -143,6 +145,39 @@ $(document).ready(function() {
             type: 'success'
           }, function() {
             // 用户点击 swal 上的 按钮时刷新页面
+            location.reload();
+          });
+        }
+      });
+    });
+  });
+  // 同意按钮的点击事件
+  $('#btn-refund-agree').click(function() {
+    swal({
+      title: '确认要将款项退还给用户？',
+      type: 'warning',
+      showCancelButton: true,
+      closeOnConfirm: false,
+      confirmButtonText: "确认",
+      cancelButtonText: "取消",
+    }, function(ret){
+      // 用户点击取消，不做任何操作
+      if (!ret) {
+        return;
+      }
+      $.ajax({
+        url: '{{ route('admin.orders.handle_refund', [$order->id]) }}',
+        type: 'POST',
+        data: JSON.stringify({
+          agree: true, // 代表同意退款
+          _token: LA.token,
+        }),
+        contentType: 'application/json',
+        success: function (data) {
+          swal({
+            title: '操作成功',
+            type: 'success'
+          }, function() {
             location.reload();
           });
         }
